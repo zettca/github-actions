d856b8eb92cfc076fdb3254a612601c9a3dd632d
Bruno <zettca@users.noreply.github.com>
2024-07-04 18:29:51 +0100

    chore: test stuff

--- .github/actions/docker-test/main.ts
+++ .github/actions/docker-test/main.ts
@@ -4,12 +4,8 @@ console.log("Args:", Deno.args);
 console.log("PWD:", Deno.env.get("PWD"));
 console.log("SECRET:", Deno.env.get("SECRET"));
 
-const [path] = Deno.args;
-
-if (!path) Deno.exit(1);
-
 console.log("Files:");
 
-for await (const dirEntry of Deno.readDir(path)) {
-  console.log(dirEntry);
+for await (const dirEntry of Deno.readDir("/")) {
+  console.log(dirEntry.name);
 }
